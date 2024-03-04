a4ddd21b35d15fb1a498d712ef8f264de1393b79
ujjwalbokde <143083146+ujjwalbokde@users.noreply.github.com>
2024-03-05 01:03:50 +0530

    all

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -92,23 +92,23 @@ class Signup (View):
     def validateCustomer(self, customer):
         error_message = None
         if (not customer.first_name):
-            error_message = "Please Enter your First Name !!"
+            error_message = " Please Enter your First Name !!"
         elif len (customer.first_name) < 3:
-            error_message = 'First Name must be 3 char long or more'
+            error_message = ' First Name must be 3 char long or more'
         elif not customer.last_name:
-            error_message = 'Please Enter your Last Name'
+            error_message = ' Please Enter your Last Name'
         elif len (customer.last_name) < 3:
-            error_message = 'Last Name must be 3 char long or more'
+            error_message = ' Last Name must be 3 char long or more'
         elif not customer.phone:
-            error_message = 'Enter your Phone Number'
+            error_message = ' Enter your Phone Number'
         elif len (customer.phone) < 10:
-            error_message = 'Phone Number must be 10 char Long'
+            error_message = ' Phone Number must be 10 char Long'
         elif len (customer.password) < 5:
-            error_message = 'Password must be 5 char long'
+            error_message = ' Password must be 5 char long'
         elif len (customer.email) < 5:
-            error_message = 'Email must be 5 char long'
+            error_message = ' Email must be 5 char long'
         elif customer.isExists ():
-            error_message = 'Email Address Already Registered..'
+            error_message = ' Email Address Already Registered..'
         # saving
 
         return error_message
